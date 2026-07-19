78212dddbb95da117e3cd7ec60b42533e5875729
Zulisa Rodriguez <zrodri2338@my.sullivan.edu>
2026-07-18 22:36:44 -0400

    added code for part 3

--- Movie.cs
+++ Movie.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+/*
+Name: Zulisa Cruz
+Date Completed: July 18, 2026
+Description: This program displays a movie title and running time.
+The running time defaults to 90 minutes when it is not provided.
+Resources Used: Course materials and class notes.
+*/
+
+namespace Movie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMovie("The Lion King");
+
+            Console.WriteLine();
+
+            DisplayMovie("Avengers: Endgame", 181);
+        }
+
+        static void DisplayMovie(string movieName, int minutes = 90)
+        {
+            Console.WriteLine($"Movie: {movieName}");
+            Console.WriteLine($"Running Time: {minutes} minutes");
+        }
+    }
+}
--- obj/Debug/net10.0/Movie.AssemblyInfo.cs
+++ obj/Debug/net10.0/Movie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f16121b69cb5ff55c936f824ae34489872a76d60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
